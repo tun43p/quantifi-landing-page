a0718de0ffee3a831418156aa1fe8d959ee7a5a3
tun43p <git@tun43p.com>
2024-07-31 19:13:24 +0200

    feat: create default layout

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export default class Config {
+  static get title(): string {
+    return "QuantiFi";
+  }
+
+  static get twitter(): string {
+    return "https://x.com/databot_xyz";
+  }
+
+  static get launchDate(): Date | null {
+    return null;
+  }
+}
