5c52f3cd8cab39213e9f6e802e208b437af39a80
tun43p <git@tun43p.com>
2024-08-01 12:07:04 +0200

    fix: paths for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://quantifi-landing-page.github.io",
-  // base: "https://github.com/tun43p/quantifi-landing-page",
 });
